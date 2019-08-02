69dd26d36c8eb521c28de388fc0f4d03f4aaa429
navingenex@gmail.com <navingenex@gmail.com>
2019-08-03 01:21:49 +0530

    dynamic theme change light/dark

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ThemeService } from './theme/theme.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'portfolio';
+  constructor(
+    private themeService: ThemeService
+  ) {}
+  ngOnInit() {
+  this.themeService.setLightTheme();
+  }
+
+
+
+
+  lightTheme() {
+      this.themeService.setLightTheme();
+  }
+  darkTheme() {
+      this.themeService.setDarkTheme();
+  }
 }
